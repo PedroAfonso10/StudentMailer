5c0aa45347eab9952509a061d5b9067860cbf225
PedroAfonso10 <peedrro999@gmail.com>
2026-09-10 20:50:49 -0300

    refactor: acessa a aba 'Resultado'

--- Students/planilha.py
+++ Students/planilha.py
@@ -2,11 +2,10 @@ import openpyxl
 
 def carregar_alunos():
     planilha = openpyxl.load_workbook('Students/Students.xlsx')
-    dados = planilha.active
+    dados = planilha['Resultado']
     for linha in dados.iter_rows(min_row=2, values_only=True):
         if linha:
             nome = linha[1]
             email = linha[2]
             situacao = linha[7]
             yield nome, email, situacao
-
