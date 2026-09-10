0db0df4919a552531dee22d4b0aeecd45048366d
PedroAfonso10 <peedrro999@gmail.com>
2026-09-10 16:19:29 -0300

    refactor: Adiciona tratamento de erro no arquivo main.py

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ if __name__ == '__main__':
     load_dotenv()
     EMAIL_REMETENTE = os.getenv('EMAIL_REMETENTE')
     SENHA_APP = os.getenv('SENHA_APP')
-
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as email:
-        email.login(EMAIL_REMETENTE, SENHA_APP)
-        for msg in enviar_email(EMAIL_REMETENTE):
-            email.send_message(msg)
+    try:
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as email:
+            email.login(EMAIL_REMETENTE, SENHA_APP)
+            for msg in enviar_email(EMAIL_REMETENTE):
+                email.send_message(msg)
+    except Exception as e:  
+        print(f"ERROR: {e}")
